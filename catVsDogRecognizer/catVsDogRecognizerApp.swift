4b2f06fc69c91d3e1afe5f9f5fe5c932c6abc8ea
Jan Maluszynski <topeerz@users.noreply.github.com>
2024-12-23 12:42:45 +0100

    Create project

---
+++ catVsDogRecognizer/catVsDogRecognizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  catVsDogRecognizerApp.swift
+//  catVsDogRecognizer
+//
+//  Created by topeerz on 23/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct catVsDogRecognizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ catVsDogRecognizerTests/catVsDogRecognizerTests.swift
@@ -0,0 +1,17 @@
+//
+//  catVsDogRecognizerTests.swift
+//  catVsDogRecognizerTests
+//
+//  Created by topeerz on 23/12/2024.
+//
+
+import Testing
+@testable import catVsDogRecognizer
+
+struct catVsDogRecognizerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
