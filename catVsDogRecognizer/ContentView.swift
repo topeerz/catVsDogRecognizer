6205047fb8927c8a022109ca660ad7a1d179ddf9
Jan Maluszynski <topeerz@users.noreply.github.com>
2024-12-23 14:22:05 +0100

    Add POC for using ml for classifying some image.

--- catVsDogRecognizer/ContentView.swift
+++ catVsDogRecognizer/ContentView.swift
@@ -7,15 +7,89 @@
 
 import SwiftUI
 
+// TODO: move this outside of ui
+import CoreML
+import UIKit
+import CoreVideo
+
+extension UIImage {
+    func toCVPixelBuffer() -> CVPixelBuffer? {
+        let width = Int(self.size.width)
+        let height = Int(self.size.height)
+
+        // Create pixel buffer attributes
+        let attributes: [String: Any] = [
+            kCVPixelBufferCGImageCompatibilityKey as String: true,
+            kCVPixelBufferCGBitmapContextCompatibilityKey as String: true
+        ]
+
+        // Create the pixel buffer
+        var pixelBuffer: CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault,
+                                          width,
+                                          height,
+                                          kCVPixelFormatType_32BGRA,
+                                          attributes as CFDictionary,
+                                          &pixelBuffer)
+
+        guard status == noErr, let buffer = pixelBuffer else {
+            return nil
+        }
+
+        // Lock the pixel buffer
+        CVPixelBufferLockBaseAddress(buffer, [])
+
+        // Create a context to draw the image
+        let context = CGContext(data: CVPixelBufferGetBaseAddress(buffer),
+                                width: width,
+                                height: height,
+                                bitsPerComponent: 8,
+                                bytesPerRow: CVPixelBufferGetBytesPerRow(buffer),
+                                space: CGColorSpaceCreateDeviceRGB(),
+                                bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue)
+
+        // Draw the image into the context
+        context?.draw(self.cgImage!, in: CGRect(x: 0, y: 0, width: width, height: height))
+
+        // Unlock the pixel buffer
+        CVPixelBufferUnlockBaseAddress(buffer, [])
+
+        return buffer
+    }
+}
+
 struct ContentView: View {
+
+    @State var isCat = false
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Image("cat.1")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height: 300)
+            Text(isCat ? "Cat" : "Dog")
+
         }
         .padding()
+        .onAppear {
+            classifyImage()
+        }
+    }
+
+    private func classifyImage() {
+        // TODO: get rid of !
+        let imageToClassify = UIImage(named: "cat.1")!.toCVPixelBuffer()! // Load the image
+        let animalRecognizer = try? animalRecognizer()
+        
+        let result = try? animalRecognizer?.prediction(image: imageToClassify)
+        DispatchQueue.main.async {
+            self.isCat = result!.target == "cat"
+        }
     }
 }
 
--- readme.md
+++ readme.md
@@ -1,1 +1,8 @@
-1 I want to read image from URL and classify it as dog or cat.
+Note: model has been trainged on 202 images from https://www.kaggle.com/datasets/ashfakyeafi/cat-dog-images-for-classification/data?select=cat_dog
+_ I want to add ML which will be classifying image (initially image can be stored in resorces/hardcoded path).
+_ I want to read image from URL and classify it as dog or cat.
+_ What can I do to retrain model on-the-fly on device? (based on user feedack)
+
+Arch:
+_ Would be nice to have some kinde of MVX template for the project.
+_ When does it make sense to create separate swift package?
